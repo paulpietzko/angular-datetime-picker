ec72ee7d13ec836fae505657b02c85b2b9aa6202
paulpietzko <paul.pietzko@icloud.com>
2025-03-18 12:09:13 +0100

    date time picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DatetimeComponent } from "./datetime/datetime.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DatetimeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
