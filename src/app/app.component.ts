23146ddb25573f7d26aceb6fb633f9d0f793781a
paulpietzko <paul.pietzko@icloud.com>
2025-03-20 13:36:03 +0100

    feat: refactor examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
-import { DatetimeComponent } from "./datetime/datetime.component";
-import { OwlPickerComponent } from "./owl-picker/owl-picker.component";
+import { AfterViewInit, Component, effect, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
+import { OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_FORMATS, OwlDateTimeInputDirective } from '@danielmoncada/angular-datetime-picker';
+import { OwlDayJsDateTimeModule } from '@danielmoncada/angular-datetime-picker-dayjs-adapter';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
+// See the Day.js docs for the meaning of these formats:
+// https://day.js.org/docs/en/display/format
+export const MY_DAYJS_FORMATS = {
+  parseInput: 'l LT',
+  fullPickerInput: 'l LT',
+  datePickerInput: 'l',
+  timePickerInput: 'LT',
+  monthYearLabel: 'MMM YYYY',
+  dateA11yLabel: 'LL',
+  monthYearA11yLabel: 'MMMM YYYY',
+};
 @Component({
   selector: 'app-root',
-  imports: [DatetimeComponent, OwlPickerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    OwlDayJsDateTimeModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatFormFieldModule
+  ],
+  providers: [
+    {provide: OWL_DATE_TIME_FORMATS, useValue: MY_DAYJS_FORMATS},
+    provideAnimations(),
+  ],
 })
 export class AppComponent {
-  title = 'angular-datetime-picker';
+  dateControl = new FormControl({ value: null, disabled: true });
+
+  dtCodeInputSig = viewChild<ElementRef<HTMLInputElement>>('dtCodeInput');
+
+
+  ngAfterViewInit() {
+    console.log("on init", this.dtCodeInputSig());
+    const codeInput = this.dtCodeInputSig();
+    if (codeInput?.nativeElement)
+      codeInput.nativeElement.disabled = true;
+  }
 }
