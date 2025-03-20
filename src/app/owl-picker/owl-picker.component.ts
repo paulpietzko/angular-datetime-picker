d49ca54c9d5154644c2b4255de22f32c19fb32ac
paulpietzko <paul.pietzko@icloud.com>
2025-03-20 10:32:18 +0100

    feat: integrate Day.js adapter for datetime picker

--- src/app/owl-picker/owl-picker.component.ts
+++ src/app/owl-picker/owl-picker.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
-import { OwlDateTimeModule, OwlNativeDateTimeModule } from '@danielmoncada/angular-datetime-picker';
+import { OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_FORMATS } from '@danielmoncada/angular-datetime-picker';
+import { OwlDayJsDateTimeModule } from '@danielmoncada/angular-datetime-picker-dayjs-adapter';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+
+// See the Day.js docs for the meaning of these formats:
+// https://day.js.org/docs/en/display/format
+export const MY_DAYJS_FORMATS = {
+  parseInput: 'l LT',
+  fullPickerInput: 'l LT',
+  datePickerInput: 'l',
+  timePickerInput: 'LT',
+  monthYearLabel: 'MMM YYYY',
+  dateA11yLabel: 'LL',
+  monthYearA11yLabel: 'MMMM YYYY',
+};
 
 @Component({
   selector: 'app-owl-picker',
   imports: [
     OwlDateTimeModule,
     OwlNativeDateTimeModule,
+    OwlDateTimeModule, OwlDayJsDateTimeModule
+  ],
+  providers: [
+    {provide: OWL_DATE_TIME_FORMATS, useValue: MY_DAYJS_FORMATS},
+    provideAnimations(),
   ],
   templateUrl: './owl-picker.component.html',
   styleUrl: './owl-picker.component.scss'
